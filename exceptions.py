0e7122f69c789e8157859934dab655caabe03d10
Raul2455 <kirillmendrin245@gmail.com>
2024-06-10 08:42:12 +0300

    Внесены исправления в файлы проекта

--- exceptions.py
+++ exceptions.py
@@ -1,26 +1,27 @@
-""" 
-Этот модуль определяет пользовательские исключения 
-для обработки ошибок API и проблем с форматом ответов. 
-""" 
- 
-class EndpointError(Exception): 
-    """Исключение возникает, когда конечная точка API недоступна.""" 
- 
-    def __init__(self, message=None, response=None): 
-        """ 
-        Инициализируйте исключение 
-        с необязательным пользовательским сообщением 
-        и объектом ответа. 
-        """ 
-        if message is None and response is not None: 
-            message = (f"Конечная точка {response.url} не доступна. " 
-                       f"Код ответа API: {response.status_code}") 
-        elif message is None: 
-            message = "Ошибка доступа к конечной точке API." 
-        super().__init__(message) 
- 
- 
-class ResponseFormatError(Exception): 
-    """Исключение вызвано ошибками в формате ответа API.""" 
-    # Здесь нет необходимости переопределять конструктор __init__, 
-    # так как он ничем не отличается от базового класса Exception.
+"""
+Этот модуль определяет пользовательские исключения.
+для обработки ошибок API и проблем с форматом ответов.
+"""
+
+
+class EndpointError(Exception):
+    """Исключение возникает, когда конечная точка API недоступна."""
+
+    def __init__(self, message=None, response=None):
+        """
+        Инициализируйте исключение.
+        с необязательным пользовательским сообщением
+        и объектом ответа.
+        """
+        if message is None and response is not None:
+            message = (f"Конечная точка {response.url} не доступна. "
+                       f"Код ответа API: {response.status_code}")
+        elif message is None:
+            message = "Ошибка доступа к конечной точке API."
+        super().__init__(message)
+
+
+class ResponseFormatError(Exception):
+    """Исключение вызвано ошибками в формате ответа API."""
+
+    pass
